5bbec433a165101eb8c26362acd2d67810fe70f0
AbuJestokiy <githubabdullarich@gmail.com>
2022-08-03 17:37:00 +0300

    Add Header

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="Wrapper clear">
+        <header className='d-flex justify-between align-center p-40'>
+          <div className="d-flex align-center">
+            <img width={40} height={40} src='/img/logo.png'/>
+              <div>
+                <h3 className='text-uppercase'>React Sneakers</h3>
+                <p>Магазин лучших кроссовок</p>
+              </div>
+            </div>
+          <ul className="d-flex">
+            <li className='mr-30'>
+              <img width={18} height={18} src='/img/Vector.png'/>
+              <span>1205 руб.</span>
+                </li>
+                  <li>
+                    <img width={18} height={18} src="/img/user.png"/>
+              </li>
+          </ul>
+        </header>
+        <div className="content p-40">
+          <h1>Все кроссовки</h1>
+        </div>
+    </div>
+  );
+}
+
+export default App;

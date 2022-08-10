c4361d80d7bbccc54e0fddae7b76c97681bcb8a8
AbuJestokiy <githubabdullarich@gmail.com>
2022-08-10 17:53:59 +0300

    korzina full

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,61 @@ import './index.css'
 function App() {
   return (
     <div className="Wrapper clear">
-      <div className="overlay">
+      <div style={{display:'none'}} className="overlay">
         <div className="drawer">
-          <h2>Корзина</h2>
+          <h2 className='d-flex justify-between mb-30 cu-p'>
+            Корзина <img className='removeBtn' src="/img/x.png" alt="Remove" />
+            </h2>
 
-          <div className="cartItem d-flex align-center">
-            <img 
-            className='mr-20'
-            height={70}
-            width={70}
-            src="/img/1.png" 
-            alt="Sneakers"
-             />
+          <div className="items">
 
-            <div className='mr-20'>
+          <div className="cartItem d-flex align-center mb-20">
+              <div style={{backgroundImage: 'url(/img/1.png)'}} className="cartItemImg"></div>
+
+            <div className='mr-20 flex'>
               <p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
               <b>12 999 руб.</b>
             </div>
             <img className='removeBtn' src="/img/x.png" alt="Remove" />
+          </div>
+
+          <div className="cartItem d-flex align-center">
+              <div style={{backgroundImage: 'url(/img/1.png)'}} className="cartItemImg"></div>
+
+            <div className='mr-20 flex'>
+              <p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className='removeBtn' src="/img/x.png" alt="Remove" />
+          </div>
+
+          <div className="cartItem d-flex align-center mt-20 mb-20">
+              <div style={{backgroundImage: 'url(/img/1.png)'}} className="cartItemImg"></div>
+
+            <div className='mr-20 flex'>
+              <p className='mb-5'>Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className='removeBtn' src="/img/x.png" alt="Remove" />
+          </div>
+
+          
+          </div>
+
+          <div className="cartTotalBlock">
+          <ul>
+            <li>
+              <span>Итого:</span>
+              <div></div>
+              <b>21 489 руб.</b>
+            </li>
+            <li>
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>1074 рубю</b>
+            </li>
+          </ul>
+          <button className='greenButton'>Оформить заказ <img src="/img/strelka.png" alt="" /></button>
           </div>
         </div>
       </div>
@@ -37,11 +74,11 @@ function App() {
             </div>
           <ul className="d-flex">
             <li className='mr-30'>
-              <img width={18} height={18} src='/img/Vector.png'/>
+              <img width={18} height={18} src='/img/korzina.png'/>
               <span>1205 руб.</span>
                 </li>
                   <li>
-                    <img width={18} height={18} src="/img/user.png"/>
+                    <img width={18} height={18} src="/img/odam.png"/>
               </li>
           </ul>
         </header>
@@ -114,6 +151,8 @@ function App() {
             </button>
             </div>
           </div>
+
+          
           </div>
         </div>
     </div>

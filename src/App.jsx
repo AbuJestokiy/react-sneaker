2a869c0213b3edc7a33014153d37b8c97abbedb0
AbuJestokiy <githubabdullarich@gmail.com>
2022-08-15 17:40:07 +0300

    map

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import './index.css'
 import Card from './Card'
 import Header from './Header'
 import Drawer from './Drawer'
+
+const arr = [
+  { name:'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl:'/img/1.png'},
+  {name:'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl:'/img/4.png'},
+  {name:'Кроссовки Puma X Aka Boku Future Rider', price: 8499, imageUrl:'/img/3.png'},
+  {name:'Мужские Кроссовки Nike Blazer Mid Suede', price: 8999, imageUrl:'/img/2.png'}
+]
+
 function App() {
   return (
     <div className="Wrapper clear">
@@ -10,18 +18,21 @@ function App() {
         <div className="content p-40">
           <div className='d-flex align-center justify-between mb-40'>
             <h1>Все кроссовки</h1>
-            <div className='search-block d-flex'>
+            <div className='search-block'>
               <img src="/img/search.png" alt="Search" />
               <input placeholder='Поиск...' />
             </div>
           </div>
 
           <div className='d-flex '>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            
+           {arr.map((obj) => (
+            <Card
+            title={obj.name}
+            price={obj.price}
+            imageUrl={obj.imageUrl}
+            onClick={() => console.log(obj)}
+            />
+           ))}
           </div>
         </div>
     </div>
